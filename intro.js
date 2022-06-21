840d485dbc557d810e7fd2b1a603bdbbec9da22b
Chinecherem Nduka <chinechnuel@gmail.com>
2022-06-21 11:25:51 +0100

    Intro to JavaScript

---
+++ intro.js
@@ -0,0 +1,7 @@
+//Write a javascript code that prints out your name, height, and country on the screen
+var name = "Chinecherem Nduka";
+var height = 5.85;
+var country = "Nigeria";
+console.log("My name is:",name);
+console.log("I am ",height,"m tall");
+console.log("I am from:",country);
